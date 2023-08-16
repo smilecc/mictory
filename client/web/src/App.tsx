355afb5e1ef14c42e86465cd9ebb7eb2d171ed37
Can <sxcuican@163.com>
2023-08-17 02:06:20 +0800

    feat(framework): init

---
+++ client/web/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
