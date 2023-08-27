28ff4c05cd38d6666355a6272c54dbfab20d71a8
Can <sxcuican@hotmail.com>
2023-08-27 12:00:21 +0800

    feat(gitignore): 忽略git ignore

--- server/src/@generated/prisma/user-session-state.enum.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from '@nestjs/graphql';
-
-export enum UserSessionState {
-    ONLINE = "ONLINE",
-    OFFLINE = "OFFLINE"
-}
-
-
-registerEnumType(UserSessionState, { name: 'UserSessionState', description: undefined })
