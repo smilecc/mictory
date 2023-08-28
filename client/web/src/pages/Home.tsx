b8612949c38da0d3be007d2c8b9544a74aca3da4
Can <sxcuican@hotmail.com>
2023-08-28 13:14:59 +0800

    WIP: feat(apollo): add schema

--- client/web/src/pages/Home.tsx
+++ client/web/src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import { useCommonStore } from "@/stores";
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { gql } from "@/@generated";
 
 export const HomePage: React.FC = () => {
   const commonStore = useCommonStore();
+  const TEST = gql(/* GraphQL */ `query`);
   return (
     <div
       onClick={() => {
-        commonStore.setThemeDarkMode(
-          commonStore.themeDarkMode === "dark" ? "light" : "dark"
-        );
+        commonStore.setThemeDarkMode(commonStore.themeDarkMode === "dark" ? "light" : "dark");
       }}
     >
       hello
