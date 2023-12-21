8f2c2894c5e5a4abc4339fe763a346ca17f1baca
SaltedFish <smilexcc@gmail.com>
2023-12-22 01:30:12 +0800

    chore: change logo

--- packages/client-desktop/src/main/index.ts
+++ packages/client-desktop/src/main/index.ts
@@ -11,7 +11,7 @@ function createWindow(): void {
     height: 900,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
